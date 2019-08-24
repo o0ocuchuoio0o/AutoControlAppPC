5793c7b01e1cc65ae91a0bc639dbe68c02893832
o0ocuchuoio0o <tranthangtin1@gmail.com>
2019-08-24 11:41:57 +0700

    a
    a

--- AutoControlAppPC/AutoControlAppPC/Form1.cs
+++ AutoControlAppPC/AutoControlAppPC/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace AutoControlAppPC
 {
@@ -14,6 +15,58 @@ namespace AutoControlAppPC
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C ping -t google.com";
+            Process.Start("CMD.exe",strCmdText);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C ping -t google.com";
+            Process p = new Process();
+            p.StartInfo.FileName = "CMD.exe";
+            p.StartInfo.Arguments = strCmdText;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+
+            p.Kill();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C ping -t google.com";
+            Process cmd = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = strCmdText;
+         //   startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardOutput = true;
+
+
+
+            cmd.StartInfo = startInfo;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(strCmdText);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            MessageBox.Show(cmd.StandardOutput.ReadToEnd());
+
         }
     }
 }
